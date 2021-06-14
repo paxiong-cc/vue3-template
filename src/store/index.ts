5b4843e958d4c11dc01d40be4b75f608ed8f23e1
Administrator <576303283@qq.com>
2021-06-14 15:24:44 +0800

    add: 初始化vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { createStore } from 'vuex'
+import user, { UserProps } from './modules/user'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+
+export interface GlobalDataProps {
+  user: UserProps;
+}
+
+const store = createStore({
   modules: {
+    user
   }
 })
+
+export default store
